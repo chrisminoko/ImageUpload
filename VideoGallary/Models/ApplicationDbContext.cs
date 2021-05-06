cea15c726850fda8de7fe3b040c57352fc518cb7
M Edoka (21750647) <21750647@dut4life.ac.za>
2021-05-06 13:39:16 -0700

    Add project files.

---
+++ VideoGallary/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoGallary.Models
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public ApplicationDbContext()
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
